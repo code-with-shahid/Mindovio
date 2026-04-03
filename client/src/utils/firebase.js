d89cba757ef15a3b01acab639e64c582a2dac7e1
shahid <mdafridishahid8910@gmail.com>
2026-04-04 01:07:11 +0530

    first commit

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "authexamnotes-f0c26.firebaseapp.com",
+  projectId: "authexamnotes-f0c26",
+  storageBucket: "authexamnotes-f0c26.firebasestorage.app",
+  messagingSenderId: "665072514968",
+  appId: "1:665072514968:web:2ad2bcabe1e11c0ca05ded"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
