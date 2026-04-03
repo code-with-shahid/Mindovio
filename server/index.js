4afb554d528c304a160035d7ac2060d4e86dd20d
Shahid Afridi <150443994+code-with-shahid@users.noreply.github.com>
2026-04-04 01:29:22 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotesaiclient-90vb.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
